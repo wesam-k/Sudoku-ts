76d3e2580dcf19ce4c055e4b26f5076f1d17e834
wesam-k <wesamalkfiry@gmail.com>
2021-07-06 17:55:00 +0200

    add build

---
+++ build/precache-manifest.e6e68682f81546ed38b8122d9aa1c814.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46bebd151ab65c477c7aefb7ad36a3c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65c8dc62c4fa5c7ddd26",
+    "url": "/static/js/2.7cb99149.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7cb99149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a64e2ee134257411eaa",
+    "url": "/static/js/main.0609025a.chunk.js"
+  },
+  {
+    "revision": "785612ead79d39e6d0c6",
+    "url": "/static/js/runtime-main.3f6a2940.js"
+  }
+]);
